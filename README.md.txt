def4ab0524cc8c79b7c7dfa7df8193a2fc2a3f14
Shildric <hildrich.salmon@ynov.com>
2024-11-09 12:17:09 +0100

    first commit

---
+++ README.md.txt
@@ -0,0 +1,3 @@
+Small lua code for Transformice
+
+It allows mices to use snowballs pressing the spacebar
---
+++ tfm.lua
@@ -0,0 +1,100 @@
+--[[
+########################## setup ##########################
+]]--
+
+function main() -- Fonction de lancement du module
+   tfm.exec.disableAutoNewGame(true)
+   -- tfm.exec.disableAutoShaman(true)
+
+   mapList = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 17, 14, 33, 104 } -- Création de la liste de cartes
+   lastSnowball = {}
+   isCrouched = {}
+
+   for playerName in pairs(tfm.get.room.playerList) do
+        eventNewPlayer(playerName)
+    end
+
+    tfm.exec.newGame(mapList[math.random(#mapList)])
+end
+
+function eventNewPlayer(playerName)
+    system.bindMouse(playerName, true)
+    tfm.exec.bindKeyboard(playerName, 32, true, true)  -- espace
+    tfm.exec.bindKeyboard(playerName, 40, true, true)  -- flèche bas
+    tfm.exec.bindKeyboard(playerName, 40, false, true) -- flèche bas
+    tfm.exec.bindKeyboard(playerName, 83, true, true)  -- S
+    tfm.exec.bindKeyboard(playerName, 83, false, true) -- S
+    lastSnowball[playerName] = os.time()
+    isCrouched[playerName] = false
+end
+
+function eventLoop(timePast, timeLeft)
+   if timeLeft <= 0 then
+       tfm.exec.newGame( mapList[math.random(#mapList)] )
+   end
+end
+
+function eventPlayerDied(name)
+   if playerLeft() == 0 then
+       tfm.exec.newGame( mapList[math.random(#mapList)] )
+   end
+end
+
+function eventPlayerWon(name, timeBegin, timeRes)
+   if playerLeft() == 0 then
+       tfm.exec.newGame( mapList[math.random(#mapList)] )
+   end
+end
+
+function playerLeft()
+   local i = 0
+   for _, prop in pairs(tfm.get.room.playerList) do
+       if not prop.isDead then
+           i = i+1
+       end
+   end
+   return i
+end
+
+--[[
+########################## game ##########################
+]]--
+
+function eventKeyboard(playerName, key, down, x, y)
+
+    if (key==32 and not tfm.get.room.playerList[playerName].isShaman and not tfm.get.room.playerList[playerName].isDead and os.time()-lastSnowball[playerName]>=1000) then
+        if (tfm.get.room.playerList[playerName].isFacingRight) then
+            if (isCrouched[playerName]) then
+                tfm.exec.addShamanObject(34, x+15, y, 0, 15, 0, false)
+            else
+                tfm.exec.addShamanObject(34, x+15, y-5, 0, 15, -5, false)
+            end
+        else
+            if (isCrouched[playerName]) then
+                tfm.exec.addShamanObject(34, x-15, y, 0, -15, 0, false)
+            else
+                tfm.exec.addShamanObject(34, x-15, y-5, 0, -15, -5, false)
+            end
+        end
+        lastSnowball[playerName] = os.time()
+    end
+
+    if (key==40 or key==83) then
+        if (down) then
+            isCrouched[playerName] = true
+        else
+            isCrouched[playerName] = false
+        end
+    end
+end
+
+function eventMouse(playerName, xMousePosition, yMousePosition)
+    if (tfm.get.room.playerList[playerName].isShaman ) then
+        tfm.exec.snow(5,10)
+    end
+end
+main()
+
+--[[
+https://transformice.fandom.com/wiki/List_of_objects_by_ID  
+]]--
